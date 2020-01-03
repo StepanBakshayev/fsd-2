90b002ed4e356a627651794eb834bb63e7e59c50
Stepan Bakshayev <step2back@gmail.com>
2020-01-03 19:09:07 +0700

    add sass for colors, use sass in pug

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
 const fs = require('fs')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 const PAGES_DIR = path.join(__dirname, 'pages')
@@ -10,10 +11,14 @@ const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.p
 
 config = {
     entry: {
+        'markup/ui-kit': path.resolve(__dirname, './markup/ui-kit.scss'),
         ...Object.fromEntries(PAGES.map(page => [page.split('.')[0], `${PAGES_DIR}/${page}`]))
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+          filename: 'css/[name].[hash].css',
+        }),
         ...PAGES.map(page => new HtmlWebpackPlugin({
             template: `${PAGES_DIR}/${page}`,
               filename: `./${page.replace(/\.pug/,'.html')}`
@@ -24,6 +29,19 @@ config = {
             {
                 test: /\.pug$/,
                 loader: 'pug-loader',
+            }, {
+              test: /\.scss$/,
+              use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                  loader: 'css-loader',
+                  options: { sourceMap: true }
+                },{
+                  loader: 'sass-loader',
+                  options: { sourceMap: true }
+                }
+              ]
             }
         ]
     }
